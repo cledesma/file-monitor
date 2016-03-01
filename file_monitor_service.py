8520c13f47b52655a92ec32d4a46761e0dc5a40b
Carlo Ledesma <cledesma@ewise.com>
2016-03-01 19:13:23 +0800

    Added tinys3

--- file_monitor_service.py
+++ file_monitor_service.py
@@ -2,6 +2,7 @@ import os
 import smtplib
 import thread
 import logging, logging.config
+import tinys3
 
 class FileMonitorService:
 
@@ -20,14 +21,28 @@ class FileMonitorService:
         if removed: logging.info("Removed: " + "\n".join(removed))
         if added:
             logging.info("Added: " + "\n".join(added))
-            # thread.start_new_thread(self.run_ftp_mail_thread, ())
-            thread.start_new_thread(self.run_ftp_mail_thread, ())
+            for file in added:
+                thread.start_new_thread(self.run_ftp_mail_thread, (file))
         self.before = after
         logging.info("*** end monitor_directory ***")
 
-    def run_ftp_mail_thread(self):
+    def run_ftp_mail_thread(self, file):
+        logging.info("begin run_ftp_mail_thread")
+        logging.info("file: " + file)
+        s3_access = os.environ['FILE_MONITOR_S3_ACCESS_KEY']
+        s3_secret = os.environ['FILE_MONITOR_S3_SECRET_KEY']
+        s3_bucket = os.environ['FILE_MONITOR_S3_BUCKET']
+        s3_endpoint = os.environ['FILE_MONITOR_S3_ENDPOINT']
+        logging.info("s3_bucket: " + s3_bucket)
+        logging.info("s3_endpoint: " + s3_endpoint)
+        conn = tinys3.Connection(s3_access,s3_secret,tls=True,endpoint=s3_endpoint)
+        f = open(file,'rb')
+        link = conn.upload(file,f,s3_bucket)
+        logging.info("S3 Link: " + link)
+
         url = "http://www.ewise.com" #TODO
         self.send_mail(url)
+        logging.info("end run_ftp_mail_thread")
 
     def send_mail(self,url):
         try: 
@@ -38,7 +53,7 @@ class FileMonitorService:
             email_port = os.environ['FILE_MONITOR_MAIL_PORT']
             email_recipient = os.environ['FILE_MONITOR_MAIL_RECIPIENT']
             logging.info("mail username: " + email_username)
-            logging.info("mail password: **********")
+            logging.info("mail password: **********l")
             logging.info("mail host: " + email_host)
             logging.info("mail.port: " + email_port)
             logging.info("mail recipient: " + email_recipient)
